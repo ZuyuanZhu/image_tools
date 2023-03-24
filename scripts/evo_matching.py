828a6d3a5a4b159b67beddb4c545912e21df6314
Zuyuan Zhu <zuyuanzhu@gmail.com>
2023-03-24 13:07:25 +0000

    reverse timestamp

---
+++ scripts/evo_matching.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+
+import numpy as np
+from evo.tools.file_interface import read_tum_trajectory_file, write_tum_trajectory_file
+from evo.core import trajectory
+
+
+base_path = "/home/zuyuan/rasberry_ws/src/data_kitti/OUTPUT/Traj_Vis/seq02_2-3.5_12-13/"
+est_file = base_path + "KF_GBA_1.csv"
+sorted_tum_file = base_path + "KF_GBA_1_sorted.csv"
+
+est_traj = read_tum_trajectory_file(est_file)
+
+sorted_indices = np.argsort(est_traj.timestamps)
+sorted_timestamps = est_traj.timestamps[sorted_indices]
+sorted_poses = [est_traj.poses_se3[i] for i in sorted_indices]
+sorted_traj = trajectory.PoseTrajectory3D(poses_se3=sorted_poses, timestamps=sorted_timestamps)
+
+write_tum_trajectory_file(sorted_tum_file, sorted_traj)
+
