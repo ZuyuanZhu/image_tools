28a682d461769aa55317ef2e64cd15548fcab497
Zuyuan Zhu <zuyuanzhu@gmail.com>
2023-04-04 17:15:09 +0100

    time continuous

--- scripts/evo_est_traj_time_continuous.py
+++ scripts/evo_est_traj_time_continuous.py
@@ -4,18 +4,64 @@
 import numpy as np
 from evo.tools.file_interface import read_tum_trajectory_file, write_tum_trajectory_file
 from evo.core import trajectory
+import csv
 
 
-base_path = "/home/zuyuan/rasberry_ws/src/data_kitti/OUTPUT/Traj_Vis/seq00_agentA0-320_agentB1378-1698/"
-est_file = base_path + "KF_GBA_1.csv"
-sorted_tum_file = base_path + "KF_GBA_1_sorted.csv"
+base_path = "/home/zuyuan/rasberry_ws/src/data_kitti/OUTPUT/Traj_Vis/seq00_agentA0-320_agentB1378-1698_test/"
+agentA_traj_sort = base_path + "KF_GBA_0_sorted.csv"
+agentB_traj_sort = base_path + "KF_GBA_1_sorted.csv"
+groundTruth_orig = base_path + "00_time_poses_selected.csv"
 
-est_traj = read_tum_trajectory_file(est_file)
+agentA_traj = read_tum_trajectory_file(agentA_traj_sort)
+agentB_traj = read_tum_trajectory_file(agentB_traj_sort)
+groundTruth = read_tum_trajectory_file(groundTruth_orig)
 
-sorted_indices = np.argsort(est_traj.timestamps)
-sorted_timestamps = est_traj.timestamps[sorted_indices]
-sorted_poses = [est_traj.poses_se3[i] for i in sorted_indices]
-sorted_traj = trajectory.PoseTrajectory3D(poses_se3=sorted_poses, timestamps=sorted_timestamps)
+last_timestamp_A = agentA_traj.timestamps[-1]
+first_timestamp_B = agentB_traj.timestamps[0]
 
-write_tum_trajectory_file(sorted_tum_file, sorted_traj)
+agentB_traj.timestamps -= first_timestamp_B
+agentB_traj.timestamps = agentB_traj.timestamps + last_timestamp_A + 0.3
+
+gap_index = -1
+gap_timeStamp = -1
+for index, timeStamp in enumerate(groundTruth.timestamps):
+    if -0.01 < timeStamp - first_timestamp_B < 0.01:
+        gap_timeStamp = timeStamp
+        gap_index = index
+        break
+
+if gap_index > 0:
+    for idx in range(gap_index-1, len(groundTruth.timestamps)):
+        groundTruth.timestamps[idx] = groundTruth.timestamps[idx] - gap_timeStamp + last_timestamp_A + 0.3
+
+groundTruth_continuous = base_path + '00_time_poses_selected_con.csv'
+write_tum_trajectory_file(groundTruth_continuous, groundTruth)
+agentB_traj_con = base_path + 'KF_GBA_1_sorted_cont.csv'
+write_tum_trajectory_file(agentB_traj_con, agentB_traj)
+
+# Open both CSV files
+with open(agentA_traj_sort, 'r') as a_file, open(agentB_traj_con, 'r') as b_file:
+    # Read the contents of both files
+    a_reader = csv.reader(a_file)
+    b_reader = csv.reader(b_file)
+    a_rows = list(a_reader)
+    b_rows = list(b_reader)
+
+# Combine the rows from both files
+combined_rows = a_rows + b_rows
+
+combined = base_path + 'KF_GBA_0_1_sorted_cont.csv'
+# Write the combined rows to a new CSV file
+with open(combined, 'w', newline='') as combined_file:
+    writer = csv.writer(combined_file)
+    writer.writerows(combined_rows)
+
+print("")
+
+# sorted_indices = np.argsort(est_traj.timestamps)
+# sorted_timestamps = est_traj.timestamps[sorted_indices]
+# sorted_poses = [est_traj.poses_se3[i] for i in sorted_indices]
+# sorted_traj = trajectory.PoseTrajectory3D(poses_se3=sorted_poses, timestamps=sorted_timestamps)
+#
+# write_tum_trajectory_file(sorted_tum_file, sorted_traj)
 
