4283ab55dea64bdb4bb3146754b9cc4d32f69730
Zuyuan Zhu <zuyuanzhu@gmail.com>
2023-05-09 11:10:52 +0100

    use image name as frame id

--- scripts/kitti2bag_sync.py
+++ scripts/kitti2bag_sync.py
@@ -15,6 +15,12 @@ from copy_images_between_range import copy_images_between_range
 
 class CameraPublisher:
 
+    """
+    This script is a Python script for a ROS node that publishes images from a specified
+    folder as a camera topic. The images are read from a folder,and their corresponding
+    timestamps are read from a separate text file. The script also writes the published
+    image messages to a ROS bag file for later use.
+    """
     def __init__(self, base_path_, folder_, time_stamps_, verbose_):
         rospy.init_node('camera_publisher', anonymous=True)
         self.verbose = verbose_
@@ -31,7 +37,7 @@ class CameraPublisher:
         self.current_pos_index = 0
 
         # Create the destination folder if it doesn't exist
-        self.bag_folder_path = os.path.join(self.base_path, 'output', folder_)
+        self.bag_folder_path = os.path.join(self.base_path, 'output_frame_id', folder_)
         if not Path(self.bag_folder_path).exists():
             Path(self.bag_folder_path).mkdir()
 
@@ -77,6 +83,9 @@ class CameraPublisher:
                 # Set the sequence number for the message header
                 seq += 1
                 ros_image.header.seq = seq
+
+                # Set the frame_id of the header to the name of the image file
+                ros_image.header.frame_id, _ = os.path.splitext(self.image_files[self.current_image_index])
 
                 self.pub_cam0.publish(ros_image)
                 self.loginfo("Current image index is %d/%d " %
