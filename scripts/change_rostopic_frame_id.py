804d63252c8d6f8dcd0fa908b7823d0663a54c9b
Zuyuan Zhu <zuyuanzhu@gmail.com>
2023-07-06 15:59:34 +0100

    Assign sequence number and split rosbag

--- scripts/change_rostopic_frame_id.py
+++ scripts/change_rostopic_frame_id.py
@@ -2,50 +2,55 @@ import rospy
 import rosbag
 import numpy as np
 
+"""
+1. Assign sequence number to frame.id 
+2. Split the rosbag at the specific number
+"""
+
 # Open the bag file for reading
-bag_path = '/media/zuyuan/DATA1TB/kitti/sim/'
-bag_name = 'agent1_sim.bag'
+bag_path = '/media/zuyuan/DATA1TB/Jackal/'
+bag_name = 'jackal_zed_small_Fri_1_0.bag'
 src_bag = bag_path + bag_name
 bag = rosbag.Bag(src_bag, 'r')
 
 # Create a new bag file for writing with modified frame IDs
-new_bag = rosbag.Bag(bag_path + 'agent1_sim_frame_id.bag', 'w', compression=rosbag.Compression.NONE)
+new_bag = rosbag.Bag(bag_path + 'jackal_zed_small_Fri_1_0_agent_0_frame_id_odom.bag', 'w', compression=rosbag.Compression.NONE)
 
-# Initialize the frame ID counter
+# Initialize the frame ID counters
 frame_id_counter = 0
 frame_id_counter_pose = 0
+split_num = 2887
 
-# Initialize the position list
-positions = []
+# Flag to determine when to save the reset as a rosbag
+save_reset = False
 
 # Iterate over the messages in the original bag file
 for topic, msg, t in bag.read_messages():
     # Check if the message is from the camera topic
-    if topic == '/cam0/image_raw':
+    if topic == '/zed/zed_node/left/image_rect_color':
         # Convert the frame ID counter to a zero-padded string
         new_frame_id = str(frame_id_counter).zfill(10)
         # Modify the frame ID of the camera message
         msg.header.frame_id = new_frame_id
         # Increment the frame ID counter
         frame_id_counter += 1
-    if topic == '/leica/position':
+    elif topic == '/odometry/filtered':
         # Convert the frame ID counter to a zero-padded string
         new_frame_id_pose = str(frame_id_counter_pose).zfill(10)
         # Modify the frame ID of the position message
         msg.header.frame_id = new_frame_id_pose
         # Increment the frame ID counter
         frame_id_counter_pose += 1
-        # Save the position data
-        positions.append([msg.point.x, msg.point.y, msg.point.z])
 
     # Write the modified message to the new bag file
     new_bag.write(topic, msg, t)
 
-# Convert the positions list to a NumPy array
-positions = np.array(positions)
-
-# Save the positions array to a local file
-np.save(bag_path + 'agent1_sim_positions.npy', positions)
+    # Check if the frame ID counter reaches the desired value
+    if frame_id_counter >= split_num and frame_id_counter_pose >= split_num and not save_reset:
+        # Save the current bag as the first rosbag
+        new_bag.close()
+        save_reset = True
+        new_bag = rosbag.Bag(bag_path + 'jackal_zed_small_Fri_1_0_agent_1_frame_id_odom.bag', 'w', compression=rosbag.Compression.NONE)
 
 # Close the bag files
 bag.close()
