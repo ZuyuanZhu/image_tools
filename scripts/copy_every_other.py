307d4428f1bfbc860b1acadcedffa7de7f809908
Zuyuan Zhu <zuyuanzhu@gmail.com>
2023-08-02 15:41:27 +0100

    create dst_dir if not exist

--- scripts/copy_every_other.py
+++ scripts/copy_every_other.py
@@ -3,12 +3,13 @@
 import os
 import shutil
 
-src_dir = '/home/zuyuan/dataset/jackal/jackal_zed_small_Fri_1_0'
-dst_dir = '/home/zuyuan/dataset/jackal/jackal_zed_small_Fri_1_0_every_other'  # replace with the path to your destination directory
+src_dir = '/media/zuyuan/DATA1TB/Jackal/bags_data_campaign_july_2023/images/test2_lower_half'
+dst_dir = '/media/zuyuan/DATA1TB/Jackal/bags_data_campaign_july_2023/images/test2_lower_half_every_other'  # replace with the path to your destination directory
 image_extension = '.jpg'  # replace with your image extension
 
 # Ensure the destination directory exists
-os.makedirs(dst_dir, exist_ok=True)
+if not os.path.exists(dst_dir):
+    os.makedirs(dst_dir)
 
 # Get a sorted list of all image filenames in the source directory
 image_filenames = sorted([f for f in os.listdir(src_dir) if f.endswith(image_extension)])
