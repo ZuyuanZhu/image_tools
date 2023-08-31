cf83129daa239a15a1c6c3fb6089345095e43890
Zuyuan Zhu <zuyuanzhu@gmail.com>
2023-08-31 16:16:09 +0100

    minor: change rostopic frame id for 2nd integration

--- scripts/change_rostopic_frame_id.py
+++ scripts/change_rostopic_frame_id.py
@@ -8,17 +8,17 @@ import numpy as np
 """
 
 # Open the bag file for reading
-bag_path = '/media/zuyuan/DATA1TB/Jackal/'
-bag_name = 'jackal_zed_small_Fri_1_0.bag'
-src_bag = bag_path + bag_name
+bag_path = '/media/zuyuan/DATA1TB/Jackal/bags_data_campaign_july_2023/'
+bag_name = 'test_2_long_134_258_select'
+src_bag = bag_path + bag_name + ".bag"
 bag = rosbag.Bag(src_bag, 'r')
 
 # Create a new bag file for writing with modified frame IDs
-new_bag = rosbag.Bag(bag_path + 'jackal_zed_small_Fri_1_0_agent_0_frame_id_odom.bag', 'w', compression=rosbag.Compression.NONE)
+new_bag = rosbag.Bag(bag_path + bag_name + '_frame_id_odom.bag', 'w', compression=rosbag.Compression.NONE)
 
 # Initialize the frame ID counters
-frame_id_counter = 0
-frame_id_counter_pose = 0
+frame_id_counter = 40000
+frame_id_counter_pose = 40000
 split_num = 2887
 
 # Flag to determine when to save the reset as a rosbag
@@ -27,14 +27,14 @@ save_reset = False
 # Iterate over the messages in the original bag file
 for topic, msg, t in bag.read_messages():
     # Check if the message is from the camera topic
-    if topic == '/zed/zed_node/left/image_rect_color':
+    if topic == '/zed2/zed_node_test/left_raw/image_raw_gray':
         # Convert the frame ID counter to a zero-padded string
         new_frame_id = str(frame_id_counter).zfill(10)
         # Modify the frame ID of the camera message
         msg.header.frame_id = new_frame_id
         # Increment the frame ID counter
         frame_id_counter += 1
-    elif topic == '/odometry/filtered':
+    elif topic == '/zed2/zed_node_test/odom':
         # Convert the frame ID counter to a zero-padded string
         new_frame_id_pose = str(frame_id_counter_pose).zfill(10)
         # Modify the frame ID of the position message
@@ -45,12 +45,12 @@ for topic, msg, t in bag.read_messages():
     # Write the modified message to the new bag file
     new_bag.write(topic, msg, t)
 
-    # Check if the frame ID counter reaches the desired value
-    if frame_id_counter >= split_num and frame_id_counter_pose >= split_num and not save_reset:
-        # Save the current bag as the first rosbag
-        new_bag.close()
-        save_reset = True
-        new_bag = rosbag.Bag(bag_path + 'jackal_zed_small_Fri_1_0_agent_1_frame_id_odom.bag', 'w', compression=rosbag.Compression.NONE)
+    # # Check if the frame ID counter reaches the desired value
+    # if frame_id_counter >= split_num and frame_id_counter_pose >= split_num and not save_reset:
+    #     # Save the current bag as the first rosbag
+    #     new_bag.close()
+    #     save_reset = True
+    #     new_bag = rosbag.Bag(bag_path + 'jackal_zed_small_Fri_1_0_agent_1_frame_id_odom.bag', 'w', compression=rosbag.Compression.NONE)
 
 # Close the bag files
 bag.close()
